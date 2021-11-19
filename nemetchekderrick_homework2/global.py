b39551e3ff960ad0d34c9c5ddf17898f96b78738
Zuke ace <zuke0000@gmail.com>
2021-11-19 02:36:13 -0700

    zipped contents to be uploaded

---
+++ nemetchekderrick_homework2/global.py
@@ -0,0 +1,44 @@
+import math
+sequences = []
+
+# add strings from fasta file that don't contain 'seq'
+with open('dotplot-example.fa') as file:
+    lines = file.readlines()
+    for line in lines:
+        if (line.find('seq') != 1):
+            sequences.append(line)    
+
+#PARAMTERS
+gapPenalty = 1
+mismatchPenality = 1
+matchPenality = 1
+
+
+firstSequence = '0' + sequences[0]
+secondSequence = "0" + sequences[1]
+row, col = (len(firstSequence)), (len(secondSequence))
+
+matrix = [[0 for x in range(col)] for y in range(row)] 
+
+
+## setup the initial negative values on the bound areas
+for i in range(len(firstSequence)):
+    for j in range(len(secondSequence)):
+        if (i == 0 or j == 0):
+            matrix[i][j] = 0 + (-6 * max(j,i))
+
+for i in range(len(firstSequence)):
+    for j in range(len(secondSequence)):
+        if (i != 0 and j != 0): # Make sure its not a bound area
+            if (firstSequence[i] == secondSequence[j]):
+                topleft = matrix[i-1][j-1] + matchPenality
+            else: 
+                topleft = matrix[i-1][j-1] - mismatchPenality
+            top = matrix[i-1][j] - gapPenalty
+            left = matrix[i][j-1] - gapPenalty
+            matrix[i][j] = max(topleft, top, left)
+del matrix[len(firstSequence) - 1]
+
+print('The score is', matrix[len(firstSequence)-2][len(secondSequence)-1])
+#for i in range(len(firstSequence ) - 1):
+ #   print(matrix[i])
